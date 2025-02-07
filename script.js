40e1b6b09a658e28f7ba25ef557e0c4d72417342
Mordecai <56131893+MordecaiVI@users.noreply.github.com>
2025-02-07 01:00:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.getElementById("toggle-btn").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
-    document.getElementById("theme-image").src = 
-        document.body.classList.contains("dark-mode") 
-        ? "images/image2.jpg" 
-        : "images/image1.jpg";
 });
