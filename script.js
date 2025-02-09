92904f6a04017b4528a6b045fa89b1ba8f209fea
Mordecai <56131893+MordecaiVI@users.noreply.github.com>
2025-02-09 20:27:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+document.getElementById("toggle-btn").addEventListener("click", function() {
+    let img = document.getElementById("image");
+    img.src = img.src.includes("images/image1.jpg") ? "images/image2.jpg" : "images/image1.jpg";
+});
